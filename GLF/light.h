39ef53b66e251da622d99513d2ea2fcd0c3b5be6
SG master <1363028533@qq.com>
2017-12-20 14:07:53 +0800

    Update.

---
+++ GLF/light.h
@@ -0,0 +1,57 @@
+#ifndef GLF_LIGHT
+#define GLF_LIGHT
+#include "main.h"
+
+class Light {
+private:
+	GLuint id;
+
+public:
+	GLuint frameHandle, textureHandle;
+	glm::mat4 lightMatrix;
+
+	glm::vec3 pos;
+	glm::vec3 diffuse, ambient;
+	GLfloat specular;
+
+	Light(GLuint ID) : id(ID) {};
+	Light(GLuint ID, glm::vec3 position, glm::vec3 diff, glm::vec3 amb, GLfloat spec) {
+		id = ID;
+		
+		pos = position;
+		diffuse = diff;
+		ambient = amb;
+		specular = spec;
+		glGenFramebuffers(1, &frameHandle);
+		glGenTextures(1, &textureHandle);
+
+		glBindTexture(GL_TEXTURE_2D, textureHandle);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+		float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
+		glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+		glBindFramebuffer(GL_FRAMEBUFFER, frameHandle);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, textureHandle, 0);
+
+		glDrawBuffer(GL_NONE);
+		glReadBuffer(GL_NONE);
+		glBindFramebuffer(GL_FRAMEBUFFER, NULL);
+	};
+	Light(const Light &l) {
+		pos = l.pos;
+		diffuse = l.diffuse;
+		ambient = l.ambient;
+		specular = l.specular;
+		textureHandle = l.textureHandle;
+		frameHandle = l.frameHandle;
+		id = l.id;
+	};
+
+	void genShadow();
+};
+
+#endif
